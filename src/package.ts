315da45173119bc81065c03232e1307e9c5282ab
Dima <crystalbit@mail.ru>
2024-06-09 12:41:12 +0200

    package 1.0.0

--- src/package.ts
+++ src/package.ts
@@ -1,1 +1,66 @@
-export const a = 5;
+type AccordionItem = {
+  title: string;
+  content: string;
+};
+
+export type AccordionData = Array<AccordionItem>;
+
+export type AccordionProps = {
+  title: string;
+};
+
+const drawMapping = (index: number, title: string, data: AccordionData) => {
+  console.clear();
+  const rawArray: Array<AccordionItem> = [...data];
+  rawArray.unshift({ title, content: "" });
+  for (let i = 0; i < rawArray.length; i++) {
+    if (i === index) {
+      console.log(`❯ ${rawArray[i].title}`);
+      if (rawArray[i].content) {
+        rawArray[i].content.split('\n').forEach((line) => {
+          console.log(`    ${line}`);
+        });
+      }
+    } else {
+      console.log(`  ${rawArray[i].title}`);
+    }
+  }
+  process.stderr.write('\x1B[?25l'); // hide cursor
+}
+
+const UP_CODE = 65;
+const DOWN_CODE = 66;
+
+export const showAccordion = (data: AccordionData, props: AccordionProps) => {
+  let index = 0;
+  (() => {
+    drawMapping(index, props.title, data);
+    process.stdin.setRawMode(true);
+    process.stdin.resume();
+    process.stdin.setEncoding('utf8');
+
+    process.stdin.on('data', function(key) {
+      const str = key.toString();
+      let isUp = false;
+      let isDown = false;
+      if (str.length == 3) {
+        isUp = str.charCodeAt(0) === 27 && str.charCodeAt(1) === 91 && str.charCodeAt(2) === UP_CODE;
+        isDown = str.charCodeAt(0) === 27 && str.charCodeAt(1) === 91 && str.charCodeAt(2) === DOWN_CODE;
+      }
+      if (str.length == 1) {
+        if (str.charCodeAt(0) === 3) {
+          process.exit(); // ctrl-c
+        }
+      }
+      if (isUp) {
+        index = index === 0 ? 0 : index - 1;
+      }
+      if (isDown) {
+        index = index === data.length + 1 - 1 ? data.length + 1 - 1 : index + 1;
+      }
+      if (isUp || isDown) {
+        drawMapping(index, props.title, data);
+      }
+    });
+  })();
+};
--- src/test.ts
+++ src/test.ts
@@ -1,3 +1,20 @@
-import { a } from "./package";
+import { AccordionData, showAccordion } from "./package";
 
-console.log(a);
+const data: AccordionData = [
+  {
+    title: "Title 1",
+    content: "Content 1",
+  },
+  {
+    title: "Title 2",
+    content: "Content 2",
+  },
+  {
+    title: "Title 3",
+    content: "Content 3\nmore content\nгде та молодая шпана что сотрет нас с лица земли",
+  }
+];
+
+showAccordion(data, {
+  title: "Accordion Title",
+});
